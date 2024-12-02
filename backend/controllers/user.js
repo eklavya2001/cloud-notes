ef62b5ed291da7c15373497259b7343fa3da8276
eklavya2001 <92029554+eklavya2001@users.noreply.github.com>
2024-12-03 00:49:30 +0530

    installed bycrypt.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,5 @@
 const User = require("../models/user");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const CryptoJs = require("crypto-js");
 const { setUser } = require("../service/auth");
 require("dotenv").config();
