ab683f48fc33d1af74693ade39c286f932a04a3e
eklavya2001 <92029554+eklavya2001@users.noreply.github.com>
2024-12-03 10:16:45 +0530

    added dist files

--- backend/index.js
+++ backend/index.js
@@ -11,10 +11,10 @@ const cookieParser = require("cookie-parser");
 const { checkForAuthentication } = require("./middlewares/auth");
 const mongourl = process.env.MONGO_URI;
 
-app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 // app.options("*", cors()); // This will handle OPTIONS preflight requests
 
