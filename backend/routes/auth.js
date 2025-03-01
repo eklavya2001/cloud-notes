d0d90ee3c237d75434d5418cd66425f9e3eac37f
eklavya2001 <92029554+eklavya2001@users.noreply.github.com>
2025-03-02 04:55:52 +0530

    cookie issue fixed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -13,11 +13,21 @@ router.post("/signup", handleUserSignup);
 
 //login route
 router.post("/login", handleUserLogin);
-// Logout route
+// // Logout route
+// router.get("/logout", (req, res) => {
+//   res.clearCookie("uid"); // Clear the JWT token cookie
+//   res.status(200).json({ message: "Logout successful" });
+// });
 router.get("/logout", (req, res) => {
-  res.clearCookie("uid"); // Clear the JWT token cookie
+  res.clearCookie("uid", {
+    httpOnly: true,
+    secure: true, // Ensure this matches your initial cookie settings
+    sameSite: "none", // Ensure SameSite matches
+    path: "/", // Ensure path matches
+  });
   res.status(200).json({ message: "Logout successful" });
 });
+
 router.get("/check", checkForAuthentication, (req, res) => {
   res.status(200).json({ isAuthenticated: true });
 });
